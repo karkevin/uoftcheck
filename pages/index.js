65976897eaa795d38ec6dde55e1dc797a40cdd9c
Kevin Xu <karkevin@gmail.com>
2021-10-13 23:44:47 -0400

    fixes

--- pages/index.js
+++ pages/index.js
@@ -35,22 +35,25 @@ export default function Home() {
           maxW="container.md"
           bgColor="#479e7a"
           color="white"
-          borderRadius="10"
+          borderRadius="5"
           padding="5"
         >
-          <Stack spacing="2">
+          <Stack spacing="-1">
             <Stack direction="row" justify="space-between">
-              <Stack spacing="1">
+              <Stack spacing="0.5">
                 <Heading fontSize="xl">Ming Yang Xu</Heading>
-                <Text>Last updated on</Text>
-                <Heading fontSize="xl">{`${month} ${d.getDate()}, ${d.getFullYear()} ${formatAMPM(
-                  d
-                )}`}</Heading>
+                <Text fontSize="sm">Last updated on</Text>
               </Stack>
-              <Image src="/thumb.png" w="12" h="12" />
+              <Image src="/thumb.png" w="14" h="14" />
             </Stack>
+            <Heading
+              fontSize="xl"
+              mt="0"
+            >{`${month} ${d.getDate()}, ${d.getFullYear()} ${formatAMPM(
+              d
+            )}`}</Heading>
             <br />
-            <Text style={{ lineHeight: "1.15" }}>
+            <Text style={{ lineHeight: "1.15" }} fontSize="sm">
               Based on your answers, you do not report symptoms of COVID-19 and
               can continue to come to campus as planned. Please continue to
               abide by all applicable public health requirements,
@@ -62,24 +65,24 @@ export default function Home() {
               contact person has your up-to-date contact information prior to
               coming on site.
             </Text>
-            <Text>
+            <Text style={{ lineHeight: "1.15" }} fontSize="sm" pt="4">
               This self-assessment does not confirm a negative diagnosis of
               COVID-19.
             </Text>
-            <Text>
+            <Text style={{ lineHeight: "1.15" }} fontSize="sm" pt="4">
               If you are in an <u>at-risk group</u>, are receiving this 'green
               status' for the first time, and:
             </Text>
             <UnorderedList pl="10" spacing="2">
               <ListItem>
-                <Text style={{ lineHeight: "1.15" }}>
+                <Text style={{ lineHeight: "1.15" }} fontSize="sm" pt="4">
                   You are a student, please contact your local health care
                   provider or campus Health & Wellness/Health & Counseling
                   Centre before you arrive on campus/leave your residence.
                 </Text>
               </ListItem>
               <ListItem>
-                <Text style={{ lineHeight: "1.15" }}>
+                <Text style={{ lineHeight: "1.15" }} fontSize="sm">
                   You are a Faculty, Librarian, or Staff member, and require an
                   alternative work arrangement or potential accommodation,
                   please contact <u>Health & Well-Being</u>.
@@ -87,14 +90,14 @@ export default function Home() {
               </ListItem>
             </UnorderedList>
             <br />
-            <Divider size="xl" />
+            <Divider size="xl" color="white" />
             <Stack
               direction="row"
               justify="space-between"
               align="center"
               px="4"
             >
-              <Heading fontSize="lg">Reccomendation</Heading>
+              <Heading fontSize="lg">Recomendation</Heading>
               <ChevronDownIcon w={5} h={5} />
             </Stack>
           </Stack>
@@ -107,7 +110,7 @@ export default function Home() {
 function formatAMPM(date) {
   var hours = date.getHours();
   var minutes = date.getMinutes();
-  var ampm = hours >= 12 ? "pm" : "am";
+  var ampm = hours >= 12 ? "PM" : "AM";
   hours = hours % 12;
   hours = hours ? hours : 12; // the hour '0' should be '12'
   minutes = minutes < 10 ? "0" + minutes : minutes;
